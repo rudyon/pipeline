d79666cf97e472592e31fcae4da0d02e23faaffe
rudyon <rudyon@example.com>
2026-03-12 15:39:01 +0300

    get rid of compiled model prefix

--- generate.py
+++ generate.py
@@ -11,7 +11,12 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 checkpoint = torch.load(args.checkpoint, map_location=device)
 model = GPT(GPTConfig(depth=args.depth, vocab_size=50304))
-model.load_state_dict(checkpoint['_orig_mod.model'])
+state_dict = checkpoint['model']
+unwanted_prefix = '_orig_mod.'
+for k, v in list(state_dict.items()):
+    if k.startswith(unwanted_prefix):
+        state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+model.load_state_dict(state_dict)
 model.to(device)
 model.eval()
 
