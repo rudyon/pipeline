20052787f41a86f46efa6081864f953829802f1d
rudyon <rudyon@example.com>
2026-03-16 15:22:08 +0300

    less steps per experiment

--- experiment.sh
+++ experiment.sh
@@ -6,7 +6,7 @@ NUM_GPUS=${NUM_GPUS:-1}
 if [ ! "$(ls -A test_cache 2>/dev/null)" ]; then
     python prepare_data.py HuggingFaceFW/fineweb-edu -c text -C sample-10BT -m 2 --cache test_cache
 fi
-torchrun --standalone --nproc_per_node=$NUM_GPUS train.py 500 \
+torchrun --standalone --nproc_per_node=$NUM_GPUS train.py 300 \
     --depth 4 \
     --batch 32768 \
     --micro 4 \
