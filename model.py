6aef0935801ccb07a426a54e355c1f752b4e56cf
rudyon <rudyon@example.com>
2026-03-16 16:18:59 +0300

    there was something i forgot

--- model.py
+++ model.py
@@ -78,7 +78,6 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         self.transformer.wte.weight = self.lm_head.weight
-        self.apply(self._init_weights)
 
     def forward(self, idx, targets=None):
         B, T = idx.size()
