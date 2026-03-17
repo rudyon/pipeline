fe82bd689f33a9cd93d9b36fbeb03906bb616b21
rudyon <rudyon@example.com>
2026-03-17 04:40:18 +0300

    kind of cleaner experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # this is designed to run on Kaggle
 pip install -r requirements.txt -q
-NUM_GPUS=$(nvidia-smi --list-gpus 2>/dev/null | wc -l)
-NUM_GPUS=${NUM_GPUS:-1}
+
+NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
+echo "Detected $NUM_GPUS GPUs"
+
 if [ ! "$(ls -A test_cache 2>/dev/null)" ]; then
     python prepare_data.py HuggingFaceFW/fineweb-edu -c text -C sample-10BT -m 2 --cache test_cache
 fi
+
 torchrun --standalone --nproc_per_node=$NUM_GPUS train.py 300 \
     --depth 4 \
     --batch 32768 \
